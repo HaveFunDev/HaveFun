fc0a0067c03985c86a02c4c25a4ee91d094066ed
Max Mogilski <maksmogi@gmail.com>
2022-11-29 17:34:16 +0100

    Fixed rwd and created mobile version of home/navigation

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,25 +1,43 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
+	const [showNavMobile, setShowNavMobile] = useState(false);
+
+	const handleShowMobile = () => {
+		setShowNavMobile((prev) => !prev);
+	};
+
 	return (
-		<nav className={styles.navbar}>
-			<img src="public/logo.png" />
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/projects">Projects</Link>
-				</li>
-				<li>
-					<Link to="/members">Members</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-			</ul>
-		</nav>
+		<div className={styles.navbar}>
+			<img src="/logo.png" className={styles.logo} />
+			<nav
+				className={`${showNavMobile ? styles.mobile : ""} ${
+					styles.navigation
+				}`}>
+				<ul
+					onClick={() => {
+						showNavMobile && handleShowMobile();
+					}}>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/projects">Projects</Link>
+					</li>
+					<li>
+						<Link to="/members">Members</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
+			</nav>
+			<button onClick={handleShowMobile} className={styles[`nav-button`]}>
+				<img src="/nav-icon.png" alt="nav bar icon" />
+			</button>
+		</div>
 	);
 };
 
