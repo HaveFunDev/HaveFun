7dfb72aafe9f4cb658febdceb99f373a09694acb
Max Mogilski <maksmogi@gmail.com>
2022-11-30 13:24:48 +0100

    Changed folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
 import "./App.scss";
-import {
-	RouterProvider,
-	createRoutesFromElements,
-	Route,
-	createBrowserRouter,
-} from "react-router-dom";
-import Home from "./pages/home/Home";
-import Projects, { loader as projectsLoader } from "./pages/projects/Projects";
-import Members from "./pages/members/Members";
-import About from "./pages/about/About";
-import RootLayout from "./pages/root-layout/RootLayout";
-
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<RootLayout />}>
-			<Route index element={<Home />} />
-			<Route path="projects" element={<Projects />} loader={projectsLoader} />
-			<Route path="members" element={<Members />} />
-			<Route path="about" element={<About />} />
-		</Route>
-	)
-);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/main-router/MainRouter";
 
 function App() {
 	return (
