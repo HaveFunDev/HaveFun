13090e129e54d17b908b83bd2d666a731bfca42d
Max Mogilski <maksmogi@gmail.com>
2023-01-16 23:08:34 +0100

    Loader has been removed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,21 @@
 import styles from "./Home.module.scss";
 import animations from "../../animations/animations.module.scss";
-import Loader from "../../components/loader/Loader";
 import { useState } from "react";
 
 const Home = () => {
-	const [isLoaded, setIsLoaded] = useState(false);
-	console.log(isLoaded);
 	return (
 		<div className={styles.container}>
-			{!isLoaded ? (
-				<Loader />
-			) : (
-				<div className={`${styles["text-content"]} ${animations.animate}`}>
-					<h2>Welcome!</h2>
-					<p>Start having fun creating awesome projects with others!</p>
-					<a href="https://github.com/HaveFunDev" target="_blank">
-						<button>Join</button>
-					</a>
-				</div>
-			)}
+			<div className={`${styles["text-content"]} ${animations.animate}`}>
+				<h2>Welcome!</h2>
+				<p>Start having fun creating awesome projects with others!</p>
+				<a href="https://github.com/HaveFunDev" target="_blank">
+					<button>Join</button>
+				</a>
+			</div>
 			<img
 				src="/start.gif"
 				alt="purple astronaut"
 				className={animations.animate}
-				onLoad={() => setIsLoaded(true)}
 				style={{
 					animationDelay: "0.5s",
 					animationTimingFunction: "linear",
